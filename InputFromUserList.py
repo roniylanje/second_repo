2263677c8e5fceea58fb381041a0e46da6f75320
snehalsatalkar <126340310+snehalsatalkar@users.noreply.github.com>
2023-03-15 21:08:24 +0530

    Delete InputFromUserList.py

--- InputFromUserList.py
+++
@@ -1,5 +0,0 @@
-# Program to take input from the user using strip and map method
-
-n=int(input("Enter the size for the list"))
-lst=list(map(int,input("Enter the list elements").strip().split()))[:n]
-print(lst)
